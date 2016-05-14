11ccc86618572548dc34267ad24bdac915abfa42
Roman Zeyde <romanzeyde@google.com>
2016-05-14 08:36:19 +0300

    gpg: hack decryption agent

--- trezor_agent/gpg/agent.py
+++ trezor_agent/gpg/agent.py
@@ -9,8 +9,11 @@ import re
 import socket
 import subprocess as sp
 
+import ecdsa
+
 from . import decode
 from .. import util
+from .. import client, factory, formats, util
 
 log = logging.getLogger(__name__)
 
@@ -138,23 +141,73 @@ def get_keygrip(user_id):
     return re.findall(r'Keygrip = (\w+)', output)[0]
 
 
-def _main():
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s %(levelname)-10s %(message)-100s '
-                        '%(filename)s:%(lineno)d')
+def _serialize_point(data):
+    data = '{}:'.format(len(data)) + data
+    # https://www.gnupg.org/documentation/manuals/assuan/Server-responses.html
+    for c in ['%', '\n', '\r']:
+        data = data.replace(c, '%{:02X}'.format(ord(c)))
+    return '(5:value' + data + ')'
 
-    p = argparse.ArgumentParser()
-    p.add_argument('user_id')
-    args = p.parse_args()
 
-    p = decode.load_from_gpg(args.user_id)
+def server(sock_path='~/.gnupg/S.gpg-agent', version='2.1.11', agent_id='TREZOR-GPG'):
+    """Run GPG agent on a UNIX socket."""
+    client_wrapper = factory.load()
+    identity = client_wrapper.identity_type()
+    identity.proto = 'gpg'
+    identity.host = 'testing'
+    curve_name = 'nist256p1'
 
-    digest = os.urandom(32)
-    sig = sign(sock=connect(),
-               keygrip=get_keygrip(args.user_id),
-               digest=digest)
-    decode.verify_digest(p, digest, sig, 'gpg-agent signature')
+    sock_path = os.path.expanduser(sock_path)
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    if os.path.exists(sock_path):
+        os.remove(sock_path)
+    sock.bind(sock_path)
+    sock.listen(1)
+    while True:
+        log.info('waiting on %s', sock_path)
+        s, addr = sock.accept()
+        s.sendall('OK pleased to see you\n')
+        while True:
+            line = _recvline(s)
+            log.info('got: %r', line)
+            if not line:
+                break
+
+            reply = 'OK\n'
+            if line == 'GETINFO version':
+                reply = 'D {}\nOK\n'.format(version)
+            if line == 'AGENT_ID':
+                reply = 'D {}\nOK\n'.format(agent_id)
+            if line == 'PKDECRYPT':
+                s.sendall('S INQUIRE_MAXLEN 4096\nINQUIRE CIPHERTEXT\n')
+                line = _recvline(s)
+                log.info('line: %r', line)
+                prefix, line = line.split(' ', 1)
+                assert prefix == 'D'
+                exp, leftover = _parse(_unescape(line))
+                log.info('exp: %s', exp)
+                pubkey = dict(exp[1][1:])['e']
+                log.info('pubkey: %r', pubkey)
+
+                result = client_wrapper.connection.sign_identity(
+                    identity=identity,
+                    challenge_hidden=pubkey,
+                    challenge_visual='Decrypt?',
+                    ecdsa_curve_name=curve_name)
+                q = result.signature
+                assert len(q) == 65
+                assert q[:1] == b'\x04'
+                log.info('result: %r', q)
+                reply = 'D ' + _serialize_point(q) + '\nOK\n'
+                log.info('reply: %r', reply)
+                s.sendall(reply)
+                break
+
+            s.sendall(reply)
 
 
 if __name__ == '__main__':
-    _main()
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)-10s %(message)-100s '
+                        '%(filename)s:%(lineno)d')
+    server()
